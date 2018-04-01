61756e0816309adee7f58bfa85d334fe2d268790
Ahmed Oraby <es-Ahmed.Oraby@alexu.edu.eg>
2018-04-01 16:23:01 +0200

    test

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,7 +10,9 @@ module.exports = (app) => {
     app.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']
     }));
 
-    app.get('/auth/facebook/callback', passport.authenticate('facebook'));
+    app.get('/auth/facebook/callback', passport.authenticate('facebook'), (req, res) => {
+        res.send("Hey");
+    });
 
 
     app.get('/api/logout', (req, res) => {

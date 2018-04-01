09c931d0159135bd5112ac049282a3a561840f38
Ahmed Oraby <es-Ahmed.Oraby@alexu.edu.eg>
2018-04-01 17:26:13 +0200

    test

--- config/keys.js
+++ config/keys.js
@@ -6,6 +6,3 @@ if(process.env.NODE_ENV === 'production'){
     module.exports = require('./dev');
 }
 
-
-// 599610451495-1ncdfncsgvqhmtb3hm18774ts68ctc9d.apps.googleusercontent.com
-// v7B0L0YTmAA87nHokPVnv60F    secret
